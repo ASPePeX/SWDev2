64f5e2efe2eac572fc324df152e5ac80d27f4326
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2016-01-22 16:43:24 +0100

    A3 added

---
+++ SWDevAufgaben/Aufgabe3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aufgabe3
+{
+    class Program
+    {
+        public static UInt64 Fibonacci(UInt64 n)
+        {
+            UInt64 a = 0;
+            UInt64 b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (UInt64 i = 0; i < n; i++)
+            {
+                UInt64 temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        static void Main()
+        {
+            for (UInt64 i = 0; i < 100; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+        }
+    }
+}
