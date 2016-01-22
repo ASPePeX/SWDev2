dc66ee781e2dce625bafdd1dcd625b8a653e4be4
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2016-01-22 17:44:07 +0100

    A4, A5 + Test

---
+++ SWDevAufgaben/Aufgabe4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe4
+{
+    internal class Program
+    {
+        public class ParentA
+        {
+            public virtual void DoSomething()
+            {
+                Console.WriteLine("I’m a type A Parent");
+            }
+        }
+
+        public class ChildA : ParentA
+        {
+            public override void DoSomething()
+            {
+                Console.WriteLine("I’m a type A child");
+            }
+        }
+
+        public class ParentB
+        {
+            public void DoSomething()
+            {
+                Console.WriteLine("I’m a type B Parent");
+            }
+        }
+
+        public class ChildB : ParentB
+        {
+            public new void DoSomething()
+            {
+                Console.WriteLine("I’m a type B child");
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            ParentA a = new ChildA();
+            a.DoSomething();
+            ParentB b = new ChildB();
+            b.DoSomething();
+        }
+    }
+}
---
+++ SWDevAufgaben/Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var blub = new Dictionary<String, String>();
+
+            blub.Add("bla0", "blub0");
+            blub.Add("bla1", "blub1");
+            blub.Add("bla2", "blub2");
+            blub.Add("bla3", "blub3");
+            blub.Add("bla4", "blub4");
+            blub.Add("bla5", "blub5");
+
+            Console.WriteLine("Blub");
+        }
+    }
+}
