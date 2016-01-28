41513c3070a594c8e1c98f42482c093b854102d6
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2016-01-28 19:57:00 +0100

    Basic Enumerator with yield

---
+++ SWDevAufgaben/Enumerator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace Enumerator
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var bub = new StringArray(5);
+
+            bub.InsertAt(1, "bla");
+            bub.InsertAt(3, "blab");
+            bub.InsertAt(4, "blub");
+
+            foreach (var b in bub)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+
+    internal class StringArray : IEnumerable
+    {
+        private readonly string[] _stringArray;
+
+        public StringArray(int num)
+        {
+            _stringArray = new string[num];
+        }
+
+        public void InsertAt(int index, string text)
+        {
+            if (index < _stringArray.Length)
+                _stringArray[index] = text;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < _stringArray.Length; i++)
+            {
+                yield return _stringArray[i];
+            }
+        }
+    }
+}
