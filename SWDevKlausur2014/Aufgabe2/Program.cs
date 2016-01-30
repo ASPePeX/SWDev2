674d22f1f93410d850b9ba2513e547a658ff5e40
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2016-01-30 11:57:38 +0100

    more A2, added A3d

---
+++ SWDevKlausur2014/Aufgabe2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aufgabe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Group() {Name = "GroupParent"};
+
+            root.Children.Add(new Group() { Name = "GroupChild1" });
+            root.Children[0].Children.Add(new Label() { Name = "Label1" });
+            root.Children[0].Children.Add(new TextInput() { Name = "TextInput1" });
+            root.Children[0].Children.Add(new Label() { Name = "Label2" });
+            root.Children[0].Children.Add(new TextInput() { Name = "TextInput2" });
+            root.Children.Add(new Group() { Name = "GroupChild2" });
+            root.Children[1].Children.Add(new Label() { Name = "Label3" });
+            root.Children[1].Children.Add(new CheckBox() { Name = "CheckBox3" });
+            root.Children[1].Children.Add(new Image() { Name = "Image1" });
+
+            var visitor = new XMLWriterVisitor();
+
+            root.Accept(visitor);
+        }
+    }
+
+    class Group : UIElement
+    {
+    }
+
+    class Label : UIElement
+    {
+    }
+    class TextInput : UIElement
+    {
+    }
+    class CheckBox : UIElement
+    {
+    }
+    class Image : UIElement
+    {
+    }
+
+    class XMLWriterVisitor : IVisitor
+    {
+        public void Visit(UIElement visitable)
+        {
+            Console.WriteLine("<" + visitable.GetType() + " Name='" + visitable.Name + "'> /");
+        }
+    }
+
+    abstract class UIElement : IVisitable
+    {
+        public string Name { get; set; }
+        public List<UIElement> Children { get; set; }
+
+        public UIElement()
+        {
+            Children = new List<UIElement>();
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+            foreach (var child in Children)
+            {
+                child.Accept(visitor);
+            }
+        }
+    }
+
+    //interface for Visitors
+    interface IVisitor
+    {
+        void Visit(UIElement visitable);
+    }
+
+    //interface for Visitables
+    interface IVisitable
+    {
+        void Accept(IVisitor visitor);
+    }
+}
---
+++ SWDevKlausur2014/Aufgabe3d/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe3d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lab = new Label() { Name= "blub"};
+            lab.WriteToFile();
+        }
+    }
+
+
+    public class UIElement
+    {
+        public string Name { get; set; }
+
+        public virtual void WriteToFile()
+        {
+            Console.WriteLine("AAAAAAAAAAAAAAAAAAAA");
+            Console.WriteLine("AAAAAAAAAAAAAAAAAAAA");
+        }
+    }
+
+    public class Group : UIElement
+    {
+        private List<UIElement> _children;
+        public IEnumerable<UIElement> Children { get { return _children; } }
+        public void WriteToFile()
+        {
+            //file.WriteLine("<Group Name='" + Name + "'>");
+            foreach (var child in Children)
+            {
+                Console.WriteLine(child.Name);
+            }
+            //file.WriteLine("</Group>");
+        }
+    }
+
+    public class Label : UIElement
+    {
+        public void WriteToFile()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    public class TextInput : UIElement
+    {
+        public void WriteToFile()
+        {
+            //file.WriteLine("<TextInput Name='" + Name + "'/>");
+        }
+    }
+}
